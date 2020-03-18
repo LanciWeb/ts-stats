9dadc4fe2879cf0fb76dd6a8c10b2d79baf10dd5
Marco Lancellotti <marcolanciesn@gmail.com>
2020-03-18 12:31:17 +0100

    added tuple for rows

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,8 +1,19 @@
 import fs from 'fs';
 import MatchResult from './MatchResult';
 import { dateStringToDate } from './utils';
+
+//! tuple definition
+type MatchDataTuple = [
+  Date,
+  string,
+  string,
+  number,
+  number,
+  MatchResult,
+  string
+];
 export default class CsvFileReader {
-  data: Array<string[]> = [];
+  data: MatchDataTuple[] = [];
   constructor(public filename: string) {}
   public read(): void {
     this.data = fs
@@ -13,16 +24,18 @@ export default class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map((row: string[]): any => {
-        return [
-          dateStringToDate(row[0]), //date
-          row[1], //home team
-          row[2], //away team
-          parseInt(row[3]), //home goals
-          parseInt(row[4]), //away goals
-          row[5] as MatchResult, //match result //!type assertion
-          row[6] //refree
-        ];
-      });
+      .map(
+        (row: string[]): MatchDataTuple => {
+          return [
+            dateStringToDate(row[0]), //date
+            row[1], //home team
+            row[2], //away team
+            parseInt(row[3]), //home goals
+            parseInt(row[4]), //away goals
+            row[5] as MatchResult, //match result //!type assertion
+            row[6] //refree
+          ];
+        }
+      );
   }
 }
